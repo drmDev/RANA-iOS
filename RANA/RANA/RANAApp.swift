78b4bf943d991813d1df53894dc237ec9a74a9dc
Derek Monturo <dmontur1@gmail.com>
2025-04-06 16:52:57 -0400

    init project

---
+++ RANA/RANA/RANAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RANAApp.swift
+//  RANA
+//
+//  Created by Derek Monturo on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RANAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RANA/RANATests/RANATests.swift
@@ -0,0 +1,17 @@
+//
+//  RANATests.swift
+//  RANATests
+//
+//  Created by Derek Monturo on 4/6/25.
+//
+
+import Testing
+@testable import RANA
+
+struct RANATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
